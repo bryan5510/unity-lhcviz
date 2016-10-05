1711ed7183c309194cbe11c52eb5438828048446
enahsthezombie <enahsnalob@gmail.com>
2016-10-05 10:37:52 -0500

    Changed to Lazer teleporter

--- LHC/LHC/Assets/Teleporter.cs
+++ LHC/LHC/Assets/Teleporter.cs
@@ -35,18 +35,19 @@ public class Teleporter : MonoBehaviour {
 			bool wasHit = Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), out hit);
 			if(wasHit){
 				if (device.GetPressDown (SteamVR_Controller.ButtonMask.Touchpad) || device.GetPressDown (SteamVR_Controller.ButtonMask.ApplicationMenu)){
-					if (hit.collider.gameObject.tag == "Warp") {
-						Transform warpPoint = hit.collider.gameObject.transform;
-						camRig.transform.position = warpPoint.position;
-						camRig.transform.rotation = warpPoint.rotation;
+					//if (hit.collider.gameObject.tag == "Warp") {
+						Vector3 warpPoint = hit.point;
+						camRig.transform.position = warpPoint;
+						//camRig.transform.rotation = warpPoint.rotation;
 
 						Physics.Raycast (transform.position, transform.TransformDirection (Vector3.forward), out hit);
-					}
+					//}
 				}
 				lineRenderer.SetPosition (0, transform.position);
 				lineRenderer.SetPosition (1, hit.point);
 			}
 		}
+		/*
 		if(device.GetPressDown (SteamVR_Controller.ButtonMask.Touchpad) || device.GetPressDown (SteamVR_Controller.ButtonMask.ApplicationMenu)){
 			if (camRig.transform.position == startingPos) {
 				camRig.transform.position = GameObject.Find ("Warp1").transform.position;
@@ -54,6 +55,6 @@ public class Teleporter : MonoBehaviour {
 				camRig.transform.position = startingPos;
 				//camRig.transform.rotation = startingRot;
 			}
-		}
+		}*/
 	}
 }
