154995c186c9e0c54eb9db97dc2a5dd709a9eb09
enahsthezombie <enahsnalob@gmail.com>
2017-02-14 13:23:41 -0600

    Updated Scrollbar and teleport

---
+++ LHC/LHC/Assets/LMtoScrollbar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Valve.VR.InteractionSystem
+{
+	[RequireComponent (typeof(Interactable))]
+	[RequireComponent (typeof(LinearMapping))]
+	public class LMtoScrollbar : MonoBehaviour
+	{
+		LinearMapping lm;
+		Scrollbar sb;
+
+		void Awake(){
+			lm = this.GetComponent<LinearMapping> ();
+			sb = this.GetComponentInParent<Scrollbar> ();
+		}
+
+		void FixedUpdate(){
+			sb.value = 1 - lm.value;
+		}
+
+	}
+}
