e6be285b0f623447534b9a1f1d47a0d2393017cd
enahsthezombie <enahsnalob@gmail.com>
2017-05-17 14:06:31 -0500

    more work on custom controller

--- LHC/LHC/Assets/Scripts/ControllerMapping.cs
+++ LHC/LHC/Assets/Scripts/ControllerMapping.cs
@@ -19,25 +19,31 @@ public class ControllerMapping : MonoBehaviour {
 	public Transform trigger;
 
 
-	float offset = 100;
-	void HandAttachedUpdate (Hand hand){
+	float offset = 50;
+	//void HandAttachedUpdate (Hand hand){
+	void Update (){
 		if(Input.GetKeyDown(KeyCode.A)){
 			button.localPosition = new Vector3(button.localPosition.x, button.localPosition.y - 0.001f, button.localPosition.z);
 			l_grip.localPosition = new Vector3(l_grip.localPosition.x, l_grip.localPosition.y - 0.001f, l_grip.localPosition.z);
 			r_grip.localPosition = new Vector3(r_grip.localPosition.x, r_grip.localPosition.y - 0.001f, r_grip.localPosition.z);
 			sys_button.localPosition = new Vector3(sys_button.localPosition.x, sys_button.localPosition.y - 0.001f, sys_button.localPosition.z);
+			trackpad.localPosition = new Vector3(trackpad.localPosition.x, trackpad.localPosition.y - 0.001f, trackpad.localPosition.z);
+			trackpad_touch.localPosition = new Vector3(trackpad_touch.localPosition.x, trackpad_touch.localPosition.y - 0.001f, trackpad_touch.localPosition.z);
 		}
 		if(Input.GetKeyUp(KeyCode.A)){
 			button.localPosition = new Vector3(button.localPosition.x, button.localPosition.y + 0.001f, button.localPosition.z);
 			l_grip.localPosition = new Vector3(l_grip.localPosition.x, l_grip.localPosition.y + 0.001f, l_grip.localPosition.z);
 			r_grip.localPosition = new Vector3(r_grip.localPosition.x, r_grip.localPosition.y + 0.001f, r_grip.localPosition.z);
 			sys_button.localPosition = new Vector3(sys_button.localPosition.x, sys_button.localPosition.y + 0.001f, sys_button.localPosition.z);
+			trackpad.localPosition = new Vector3(trackpad.localPosition.x, trackpad.localPosition.y + 0.001f, trackpad.localPosition.z);
+			trackpad_touch.localPosition = new Vector3(trackpad_touch.localPosition.x, trackpad_touch.localPosition.y + 0.001f, trackpad_touch.localPosition.z);
 		}
 
 		// pip limits at 0.02 from mid
-		trackpad.localEulerAngles = new Vector3 (trackpad_touch.localPosition.z * offset, 0, -trackpad_touch.localPosition.x * offset);
+		trackpad.localEulerAngles = new Vector3 (Mathf.Clamp (trackpad_touch.localPosition.z*offset,-1,1), 0, -Mathf.Clamp (trackpad_touch.localPosition.x*offset,-1,1));
 
 		//trigger rot should be between 0 and -18 on the x
+		trigger.localEulerAngles = new Vector3(-9f*((Mathf.Clamp (trackpad_touch.localPosition.x*offset,-1,1)+1f)),0,0);
 
 		//hand.controller.hairTriggerDelta;
 
